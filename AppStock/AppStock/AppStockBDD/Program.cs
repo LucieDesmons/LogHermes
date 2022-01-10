7351daba66569ba0dd14ce1b9d588413829b0355
NicoCesi789 <beckernice@gmail.com>
2022-01-10 13:12:59 +0100

    Mise à jour

---
+++ AppStock/AppStock/AppStockBDD/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace AppStockBDD
+{
+    public class Article
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal note { get; set; }
+        public DateTime année { get; set; }
+        public string cépage { get; set; }
+
+    }
+public class ArticleContext : DbContext
+    {
+        public DbSet<Article> articles { get; set; }
+    }
+}
