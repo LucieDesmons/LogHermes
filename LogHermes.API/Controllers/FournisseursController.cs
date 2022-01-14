8c50d4ad43aeabb3f3493fb1e394e2856def1266
Lucie <desmons.lucie@gmail.com>
2022-01-14 16:23:14 +0100

    [Integration] Start API CRUD

---
+++ LogHermes.API/Controllers/FournisseursController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LogHermes.API.Controllers
+{
+    [Route("api/fournisseurs")]
+    [ApiController]
+    public class FournisseursController : ControllerBase
+    {
+        // GET: api/<FournisseursController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<FournisseursController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<FournisseursController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<FournisseursController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FournisseursController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
