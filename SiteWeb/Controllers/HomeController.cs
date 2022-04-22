4c4a87a5db5376ffbc5b2173ebaa40b62362086b
Lucie <desmons.lucie@gmail.com>
2022-04-22 14:32:42 +0200

    [Integration] Radios buttons in filters for types of wines

--- SiteWeb/Controllers/HomeController.cs
+++ SiteWeb/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace SiteWeb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public ActionResult RenderProducts()
+        {
+            return PartialView("_Products");
+        }
     }
 }
