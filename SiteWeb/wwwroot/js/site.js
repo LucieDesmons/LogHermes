8159544cac868e32a72cd127d42969be8a9d6820
Lucie <desmons.lucie@gmail.com>
2022-04-28 22:32:35 +0200

    [Update] card's and modal's design

--- SiteWeb/wwwroot/js/site.js
+++ SiteWeb/wwwroot/js/site.js
@@ -15,17 +15,14 @@
                 $('#myModal').modal('toggle');
 
                 //Contenu modal
-                // Modal Header
-                let strContenuModal = "<div class='modal-header'>"
-                strContenuModal += " <h4 class='modal-title'>Détails du produit " + prd.nom + "</h4>"
                     // strContenuModal += "<button type='button' class='close' data-dismiss='modal'>&times;</button>" /
-                        strContenuModal += "</div>"
                 // Modal body
                 
-                strContenuModal += "<div class='modal-body d-flex flex-column text-center justify-content-center'>"
+                let strContenuModal = "<div class='modal-body d-flex flex-column text-center justify-content-center'>"
                     strContenuModal += "<div class='row'>"
 
-                        strContenuModal += "<div class='col-6 d-flex flex-column border-right'>"
+                        strContenuModal += "<div class='col-6 d-flex flex-column justify-content-between border-right'>"
+                        strContenuModal += " <h4>Détails du produit " + prd.nom + "</h4>"
                             /*strContenuModal += " <img src=" + prd.Image + "class='h-100 w-100'>"*/
                             if (prd.quantite >= 1) {
                                 strContenuModal += "<p style='color:#35B736'> Quantité disponible : " + prd.quantite + "</p>"
@@ -49,31 +46,50 @@
 
                         strContenuModal += "</div>"
 
-                        strContenuModal += "<div class='col-6 d-flex flex-column d-flex flex-column'>"
-                            strContenuModal += "<h5> Je souhaite commander... </h5>"
-                            strContenuModal += "<p> Attention: Si vous prenez l'option 'Carton de vins' la quantité sera basée dessus. </p>"
+                        strContenuModal += "<div class='col-6 d-flex flex-column justify-content-between'>"
+                            strContenuModal += "<h4> Je souhaite commander... </h5>"
+                            strContenuModal += "<p class='m-0 p-2'> Attention: Si vous prenez l'option 'Carton de vins' la quantité sera basée dessus. </p>"
 
-                            strContenuModal += "<div class='form-check' >"
-                                strContenuModal += "<input class='form-check-input' type='radio' name='flexRadioDefault' id='flexRadioDefault1'>"
-                                strContenuModal += "<label class='form-check-label' for='flexRadioDefault1'>"
-                                strContenuModal += "Oui"
-                                strContenuModal += "</label>"
+                        strContenuModal += "<div class='row d-flex p-2'>"
+                            strContenuModal += "<div class='col-4 d-flex justify-content-center align-items-center'>"
+                                strContenuModal += "<label style='color:#f5585c; font-weight:bold;'> Carton de vins : </label>"
                             strContenuModal += "</div>"
-                            strContenuModal += "<div class='form-check' >"
-                                strContenuModal += "<input class='form-check-input' type='radio' name='flexRadioDefault' id='flexRadioDefault2'>"
-                                strContenuModal += "<label class='form-check-label' for='flexRadioDefault2'>"
-                                strContenuModal += "Non"
-                                strContenuModal += "</label>"
+                            strContenuModal += "<div class='col-8'>"
+                                strContenuModal += "<div class='form-check' >"
+                                    strContenuModal += "<input class='form-check-input' type='radio' name='flexRadioDefault' id='flexRadioDefault1'>"
+                                    strContenuModal += "<label class='form-check-label d-flex justify-content-start' for='flexRadioDefault1'>"
+                                    strContenuModal += "Oui"
+                                    strContenuModal += "</label>"
+                                strContenuModal += "</div>"
+                                strContenuModal += "<div class='form-check' >"
+                                    strContenuModal += "<input class='form-check-input' type='radio' name='flexRadioDefault' id='flexRadioDefault2'>"
+                                    strContenuModal += "<label class='form-check-label d-flex justify-content-start' for='flexRadioDefault2'>"
+                                    strContenuModal += "Non"
+                                    strContenuModal += "</label>"
+                                strContenuModal += "</div>"
                             strContenuModal += "</div>"
+                        strContenuModal += "</div>"
 
-                            strContenuModal += "<div>"
-                                strContenuModal += "<p> Quantité : </p>"
-                                strContenuModal += "<input type ='number' id ='quantiteCmd' class='form-control active' value ='1' >"
+                            strContenuModal += "<div class='row d-flex p-2'>"
+                                strContenuModal += "<div class='col-4 d-flex justify-content-center align-items-center'>"
+                                    strContenuModal += "<label> Quantité : </label>"
+                                strContenuModal += "</div>"
+                                strContenuModal += "<div class='col-8'>"
+                                    strContenuModal += "<input type ='number' id ='quantiteCmd' class='form-control active' value ='1' >"
+                                strContenuModal += "</div>"   
                             strContenuModal += "</div>"   
 
-                            strContenuModal += "<div>"
-                                strContenuModal += "<p> Prix HT : </p>"
-                                strContenuModal += "<input id ='PrixCmd' class='form-control active' value ='1' >"
+                            strContenuModal += "<div class='row d-flex p-2'>"
+                                strContenuModal += "<div class='col-4 d-flex justify-content-center align-items-center'>"
+                                    strContenuModal += "<label>  Prix HT : </label>"
+                                strContenuModal += "</div>"
+                                strContenuModal += "<div class='col-8'>"
+                                    strContenuModal += "<input id ='PrixCmd' class='form-control active' value ='1' >"
+                                strContenuModal += "</div>"
+                            strContenuModal += "</div>"    
+
+                            strContenuModal += "<div class='row p-2 mt-4'>"
+                                strContenuModal += "<button type ='button' class='btn btn-primary' data-target='#exampleModalCenter'>Ajouter au panier</button>"
                             strContenuModal += "</div>"
 
                         strContenuModal += "</div>"
@@ -84,7 +100,7 @@
                 
                 // Modal footer 
                 strContenuModal += "<div class='modal-footer'>"
-                strContenuModal += "<button type='button' class='btn btn-danger' onclick=closeModalProduit() data-dismiss='modal'>Close</button>"
+                strContenuModal += "<button type='button' class='btn btn-danger' onclick=closeModalProduit() data-dismiss='modal'>Annuler</button>"
                 strContenuModal += "</div>"
 
                 $('.modal-content').html(strContenuModal);
